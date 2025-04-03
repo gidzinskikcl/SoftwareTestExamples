4ab21ed2bfff7f4a38b58016fa3cb85a4d683c82
gidzinskikcl <k24112096@kcl.ac.uk>
2025-04-03 14:54:15 +0100

    Organise tests folder, update readme

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# SoftwareTestExamples
-This simple project was created to demonstrate my approach to software testing, utilizing a variety of test cases and techniques. It shows how I test software systems, identify defects, and ensure functionality through systematic test case creation.
+# A Demonstration of Software Testing in Java
+
+This simple project was created to demonstrate my approach to software testing in Java, utilising a variety of test cases and techniques.
 
 This project includes:
 
-- **Test Case Design:** A series of unit tests targeting specific functionality within the software, written using JUnit and Mockito for Java.
+- **Test Case/Suite Design:** A series of unit tests targeting specific functionality within the software, written using JUnit and Mockito.
 
-- **Test Suite Organization:** Grouping related test cases into test suites for efficient execution and coverage analysis.
+  - **Note:** some tests fail on purpose in order to demonstrate how tests helped detecting faults in the software.
 
 - **Fault Detection and Debugging: (to be continued)** A detailed analysis of potential software faults, their identification through tests, and explanations of how the issues were uncovered.
 
-- **Test Reporting: (to be continued)** A comprehensive report discussing testing approaches, challenges faced during testing, trade-offs made in test design, and a summary of defects found.
+- **Coverage Analysis: (to be continued)** Generating test coverage to measure how much of the software has been tested and to highlight areas that may need additional attention (VSCode feature).
 
-- **Coverage Analysis: (to be contnued)** Generation of a test coverage report to measure how much of the software has been tested and to highlight areas that may need additional attention (VSCode).
+<u>**Below I explain the software system under test and the user requirements**</u>
+
+## Brief Software Description
+
+The Shopping Cart system is an online platform designed to provide a seamless purchasing experience. It allows users to browse products, add them to their cart, and accurately calculate the total cost. The system includes several discount mechanisms, such as tiered discounts based on cart value, customer-specific pricing (Regular, Premium, or VIP), and bundle discounts for certain product combinations. Additionally, it supports time-limited promotions and coupon codes for extra savings.
---
+++ src/Tests/onlineshopping/TestOrderService.java
@@ -0,0 +1,98 @@
+package tests.onlineshopping;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import onlineshopping.CartItem;
+import onlineshopping.InventoryService;
+import onlineshopping.OrderService;
+import onlineshopping.PaymentService;
+import onlineshopping.ShoppingCart;
+
+public class TestOrderService {
+
+    private OrderService orderService;
+
+    @Test
+    public void testPlaceOrder() throws Exception {
+        PaymentService mockPaymentService = mock(PaymentService.class);
+        InventoryService mockInventoryService = mock(InventoryService.class);
+
+        ShoppingCart mockCart = mock(ShoppingCart.class);
+        String cardNumber = "1234567890123456";
+
+        CartItem mockItem = mock(CartItem.class);
+        when(mockCart.getItems()).thenReturn(Arrays.asList(mockItem));
+        when(mockCart.calculateTotal()).thenReturn(100.0);
+
+        doNothing().when(mockInventoryService).updateStock(mockItem);
+        when(mockPaymentService.processPayment(cardNumber, 100.0)).thenReturn(true);
+
+        orderService = new OrderService(mockPaymentService, mockInventoryService);
+        assertTrue("The order should be placed", orderService.placeOrder(mockCart, cardNumber));
+    }
+
+    @Test
+    public void testPlaceOrderThrowExceptionPayment() throws Exception {
+        PaymentService mockPaymentService = mock(PaymentService.class);
+        InventoryService mockInventoryService = mock(InventoryService.class);
+
+        ShoppingCart mockCart = mock(ShoppingCart.class);
+        String cardNumber = "1234567890123456";
+
+        CartItem mockItem = mock(CartItem.class);
+        when(mockCart.getItems()).thenReturn(Arrays.asList(mockItem));
+        when(mockCart.calculateTotal()).thenReturn(100.0);
+
+        doNothing().when(mockInventoryService).updateStock(mockItem);
+        when(mockPaymentService.processPayment(cardNumber, 100.0)).thenThrow(new Exception());
+
+        orderService = new OrderService(mockPaymentService, mockInventoryService);
+        assertFalse("Placing order shoud not happen", orderService.placeOrder(mockCart, cardNumber));
+    }
+
+    @Test
+    public void testPlaceOrderThrowExceptionInventory() throws Exception {
+        PaymentService mockPaymentService = mock(PaymentService.class);
+        InventoryService mockInventoryService = mock(InventoryService.class);
+
+        ShoppingCart mockCart = mock(ShoppingCart.class);
+        String cardNumber = "1234567890123456";
+
+        CartItem mockItem = mock(CartItem.class);
+        when(mockCart.getItems()).thenReturn(Arrays.asList(mockItem));
+        when(mockCart.calculateTotal()).thenReturn(100.0);
+
+        doNothing().when(mockInventoryService).updateStock(mockItem);
+        doThrow(new IllegalArgumentException()).when(mockInventoryService).updateStock(mockItem);
+
+        orderService = new OrderService(mockPaymentService, mockInventoryService);
+        assertFalse("Placing order should not hapen", orderService.placeOrder(mockCart, cardNumber));
+    }
+
+    @Test
+    public void testPlaceOrderThrowExceptionCalculateTotal() throws Exception {
+        PaymentService mockPaymentService = mock(PaymentService.class);
+        InventoryService mockInventoryService = mock(InventoryService.class);
+
+        ShoppingCart mockCart = mock(ShoppingCart.class);
+        String cardNumber = "1234567890123456";
+
+        CartItem mockItem = mock(CartItem.class);
+        when(mockCart.getItems()).thenReturn(Arrays.asList(mockItem));
+
+        doNothing().when(mockInventoryService).updateStock(mockItem);
+        doThrow(new IllegalArgumentException()).when(mockCart).calculateTotal();
+
+        orderService = new OrderService(mockPaymentService, mockInventoryService);
+        assertFalse("Placing order should not happen", orderService.placeOrder(mockCart, cardNumber));
+    }
+}
